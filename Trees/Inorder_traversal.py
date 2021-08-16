ffb4e069c82983b2ed931fe0ecdfe27a07890c22
Ashutosh Verma <ashutoshv1520@gmail.com>
2021-08-16 23:28:18 +0530

    treee

---
+++ Trees/Inorder_traversal.py
@@ -0,0 +1,22 @@
+# @author
+# Ashutosh.Verma
+
+# Problem: https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+        else:
+            return self.inorderTraversal(root.left)+[root.val]+self.inorderTraversal(root.right)
+        
