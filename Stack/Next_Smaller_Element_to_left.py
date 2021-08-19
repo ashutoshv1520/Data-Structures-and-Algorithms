c51c1f010ee39bd98ce56363cb031cab2efdba2e
Ashutosh Verma <ashutoshv1520@gmail.com>
2021-08-19 13:54:52 +0530

    stack2

--- Stack/Next_Smaller_Element_to_left.py
+++ Stack/Next_Smaller_Element_to_left.py
@@ -2,7 +2,7 @@ input=list(map(int,input().split()))
 stack=[]
 res=[]
 for i in range(len(input)):
-    while len(stack)!=0 and stack[-1]<=input[i]:
+    while len(stack)!=0 and stack[-1]>=input[i]:
         stack.pop()
     if len(stack)==0:
         res.append(-1)
---
+++ Stack/Next_Smaller_Element_to_right.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+
+
+class Solution:
+    def nextLargerElement(self,arr,n):
+        #code here
+        stack=[]
+        res=[]
+        for i in range(n-1,-1,-1):
+            while(len(stack)!=0 and stack[-1]>=arr[i]):
+                stack.pop()
+            if len(stack)==0:
+                res.append(-1)
+            else:
+                res.append(stack[-1])
+            stack.append(arr[i])
+        return res[::-1]
